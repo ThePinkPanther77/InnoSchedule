d66b8ab0835fac99915ac2895f33d81fcf2880eb
ThePinkPanther77 <hayder.sar77@gmail.com>
2023-07-18 19:41:02 +0300

    added some fixes to the branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
         {children}
-        <Footer />
       </body>
     </html>
   )
